35bd53a4545a5aec17dcc53dd1ef7128571c01f2
Sambro2105 <56916691+Sambro2105@users.noreply.github.com>
2019-10-27 18:23:29 +0500

    First Introduction

---
+++ ConvertStringToNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+//    Преобразование строки в число
+//    Вася написал код, прибавляющий к числу единичку, но он опять не работает :(
+//
+//    Немедленно помогите Васе, иначе он решит, что программирование слишком сложно для него!
+
+namespace ConvertStringToNumber
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string doubleNumber = "894376.243643";
+            double number = double.Parse(doubleNumber, CultureInfo.InvariantCulture); // Вася уверен, что ошибка где-то тут
+            Console.WriteLine(number + 1);
+        }
+    }
+}
