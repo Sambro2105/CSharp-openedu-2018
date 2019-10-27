ac144ea04f589cf6f036064459ef3d5b5e4ff898
Sambro2105 <56916691+Sambro2105@users.noreply.github.com>
2019-10-27 18:16:08 +0500

    Branching

---
+++ Pluralization/PluralizeTask.cs
@@ -0,0 +1,27 @@
+﻿/*Практика «Pluralization»
+Скачайте проект Pluralize
+
+Напишите метод, склоняющий существительное «рублей» следующее за указанным числительным.
+
+Например, для аргумента 10, метод должен вернуть «рублей», для 1 — вернуть «рубль», для 2 — «рубля».
+
+Для проверки своего решения запустите скачанный проект.*/
+
+namespace Pluralize
+{
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+			if (count % 100 == 11 || count % 100 == 12 || count % 100 == 13 || count % 100 == 14)
+				return "рублей";
+			else if (count % 10 == 0)
+				return "рублей";
+			else if (count % 10 == 1)
+				return "рубль";
+			else if (count % 10 == 2 || count % 10 == 3 || count % 10 == 4)
+				return "рубля";
+			else return "рублей";
+		}
+	}
+}
