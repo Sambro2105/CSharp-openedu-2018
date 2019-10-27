2f1f5224e3b613d6af76647a21faf38a795ea259
Sambro2105 <56916691+Sambro2105@users.noreply.github.com>
2019-10-27 18:25:45 +0500

    First Introduction

---
+++ TypeConversionErrors/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//    Ошибки преобразования типов
+//    Исправьте все ошибки преобразования типов. Читайте и переводите с английского имена переменных,
+// чтобы понять, что от вас требуется.
+
+namespace TypeConversionErrors
+{
+    class Program
+    {
+        public static void Main()
+        {
+            double pi = Math.PI;
+            int tenThousand = (int)10000L;
+            float tenThousandPi = (float)pi * tenThousand;
+            int roundedTenThousandPi = (int)Math.Round(tenThousandPi);
+            int integerPartOfTenThousandPi = (int)tenThousandPi;
+            Console.WriteLine(integerPartOfTenThousandPi);
+            Console.WriteLine(roundedTenThousandPi);
+        }
+    }
+}
